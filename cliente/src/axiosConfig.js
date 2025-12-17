7e0f63fe5bab6469c25e8ab8441fea1b0a58c9dd
Marcos24Vlad <marcosvladimir24@gmail.com>
2025-12-17 13:21:08 -0500

    correccion de axios

--- cliente/src/axiosConfig.js
+++ cliente/src/axiosConfig.js
@@ -1,9 +1,20 @@
 import axios from "axios";
- 
+
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_API_ENDPOINT}/api`, // 🔹 Ajusta según el puerto de tu servidor
-    withCredentials: true, // 🔹 Permite el envío de cookies o autenticación si es necesario
+  baseURL: `${process.env.REACT_APP_API_ENDPOINT}/api`,
+  withCredentials: true,
 });
 
+// 🔑 Interceptor para enviar el token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // ajusta el nombre
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
- 
